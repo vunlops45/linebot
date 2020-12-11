7b9ce23ceaf4c54230cabd4731642338db1ca2e5
vunlops45 <75350189+vunlops45@users.noreply.github.com>
2020-12-11 19:34:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if($arrJson['events'][0]['message']['text'] == "หวย"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $arrJson2;
+  $arrPostData['messages'][0]['text'] = $arrJson2['GoldSpot'][0]['message']['text'];
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];

1fcb6214c254d6bf7bc697e498ed55553cd15e03
vunlops45 <75350189+vunlops45@users.noreply.github.com>
2020-12-11 19:48:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if($arrJson['events'][0]['message']['text'] == "หวย"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $arrJson2['name'][0]['Update']['text'];
+  $arrPostData['messages'][0]['text'] = $content;
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];

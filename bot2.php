fc0baf2926d16dafc80c9ee536e0740b656a4eca
vunlops45 <75350189+vunlops45@users.noreply.github.com>
2021-06-23 14:18:46 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -130,5 +130,17 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
 
+function curl_get_contents($url)
+{
+    $ch=curl_init();
 
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data=curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
 ?>

989afee33132a3446e47f7a043d7da8472ccef1a
vunlops45 <75350189+vunlops45@users.noreply.github.com>
2021-06-23 12:29:20 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -84,7 +84,7 @@ $arrPostData = array();
   }else if($arrJson['events'][0]['message']['text'] == "1"){
 
  //$json = file_get_contents('http://covid19.th-stat.com/json/covid19v2/getTodayCases.json');
- $json ='{"Confirmed":225365,"Recovered":187836,"Hospitalized":35836,"Deaths":1693,"NewConfirmed":4059,"NewRecovered":2047,"NewHospitalized":1977,"NewDeaths":35,"UpdateDate":"22\/06\/2021 16:59","DevBy":"https:\/\/www.kidkarnmai.com\/"}'
+ $json ='{"Confirmed":225365,"Recovered":187836,"Hospitalized":35836,"Deaths":1693,"NewConfirmed":4059,"NewRecovered":2047,"NewHospitalized":1977,"NewDeaths":35,"UpdateDate":"22\/06\/2021 16:59","DevBy":"https:\/\/www.kidkarnmai.com\/"}';
 
  
  //$json = '{"CustomerID":"C001","Name":"Weerachai Nukitram","Email":"win.weerachai@thaicreate.com","CountryCode":"TH","Budget":"1000000","Used":"600000"}';
